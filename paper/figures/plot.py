174e20c54286609622eeee141968a2be7c5ea486
GiggleLiu <cacate0129@gmail.com>
2021-09-23 14:31:22 -0400

    update

--- paper/figures/plot.py
+++ paper/figures/plot.py
@@ -11,16 +11,17 @@ import os
 
 class PLT(object):
     def fig1(self, tp="pdf"):  # independence polynomials
-        nw = [(10, 3), (20, 4), (30, 4), (40, 5), (50, 8), (60, 8), (70, 8), (80, 10), (90, 13), (100, 13),
-            (110, 15), (120, 15), (130, 13), (140, 17), (150, 18), (160, 20), (170, 19), (180, 25), (190, 24), (200, 25)]
+        nw = [(10, 3, 7.66), (20, 4, 9.01), (30, 4,9.63), (40, 5,10.36), (50, 8,12.32), (60, 8,12.63), (70, 8,12.55), (80, 10,14.68), (90, 13,16.76), (100, 13,17.67),
+            (110, 15, 18.94), (120, 15, 20.47), (130, 13, 18.23), (140, 17, 21.57), (150, 18, 22.41), (160, 20, 26.23), (170, 19, 24.15), (180, 24, 32.04), (190, 24, 30.27), (200, 25, 32.10)]
         ns = [x[0] for x in nw]
         FS = 8
         with DataPlt(filename="fig1.%s"%tp, figsize=(10,6)) as dp:
             ax = plt.subplot(221)   # treewidth
             cornertex("(a)", ax, offset=(0,0))
-            plt.plot(ns, [x[1] for x in nw])
+            plt.plot(ns, [x[1] for x in nw], label="space complexity (tree width)")
+            plt.plot(ns, [x[2] for x in nw], label="time complexity")
             plt.xlabel("number of vertices, $|V|$")
-            plt.ylabel("tree width")
+            plt.ylabel("log2 value")
 
             ax=plt.subplot(222)   # size
             cornertex("(b)", ax, offset=(0,0))
@@ -75,13 +76,13 @@ class PLT(object):
         FS = 10
         with DataPlt(filename="fig2.%s"%tp, figsize=(8,3)) as dp:
             ax = plt.subplot(121)   # treewidth
-            cornertex("(a)", ax, offset=(0,0))
+            cornertex("(a)", ax, offset=(-0.02,0))
             plt.plot(ns, [x[1] for x in nw])
             plt.xlabel("number of vertices, $|V|$")
             plt.ylabel("tree width")
 
             ax=plt.subplot(122)   # size
-            cornertex("(b)", ax, offset=(0,0))
+            cornertex("(b)", ax, offset=(-0.02,0))
             for k,(prefix, l) in enumerate([("counting_sum", r"$\mathbb{R}$"), ("counting_all_(finitefield)", "GF$(p)$"), ("configs_all", "PN+SN")]):
                 for device in ["CPU", "GPU"]:
                     datafile = "../../benchmarks/data/maximal-"+prefix+"-r3-"+device+".dat"
@@ -99,15 +100,30 @@ class PLT(object):
 
             plt.tight_layout()
 
-    def fig3(self, tp="pdf", seed=3):  # independence polynomials
-        with DataPlt(filename="fig3.%s"%tp, figsize=(6,3)) as dp:
+    def fig3(self, tp="pdf"):  # independence polynomials
+        with DataPlt(filename="fig3.%s"%tp, figsize=(7,3)) as dp:
+            ax = plt.subplot(121)   # treewidth
+            cornertex2("(a)", ax, offset=(0,0))
+            seed = 3
             datafile = "../../democode/data/r3_n200_hamming_%d.txt"%seed
             y = np.loadtxt(datafile)
-            n = 18
+            n = np.max(np.where(y > 0)[0])
             plt.bar(range(n), y[:n], lw=1.5, color="C4", ls="-")
-            plt.xticks(range(0,n+1,2))
             plt.xlabel("Hamming distance")
             plt.ylabel("Count")
-            plt.tight_layout()
+            plt.xlim(0, 200)
+            ax.ticklabel_format(style="scientific", scilimits=(-3, 3))
+
+            ax = plt.subplot(122)   # treewidth
+            cornertex2("(b)", ax, offset=(0,0))
+            seed = 8
+            datafile = "../../democode/data/r3_n200_hamming_%d.txt"%seed
+            y = np.loadtxt(datafile)
+            n = np.max(np.where(y > 0)[0])
+            plt.bar(range(n), y[:n], lw=1.5, color="C4", ls="-")
+            plt.xlabel("Hamming distance")
+            plt.xlim(0, 200)
+
+            plt.tight_layout(h_pad=30.0)
 
 fire.Fire(PLT())
