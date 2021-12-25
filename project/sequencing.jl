fd545a022de14471a018f37722ebfe0a1d62a478
GiggleLiu <cacate0129@gmail.com>
2021-12-25 13:30:16 +0000

    update sequence

--- project/sequencing.jl
+++ project/sequencing.jl
@@ -7,7 +7,7 @@ using DelimitedFiles
 function sequencing(n; writefile, sc_target, usecuda, nslices=1)
     g = square_lattice_graph(trues(n, n))
     gp = Independence(g; optimizer=TreeSA(sc_target=sc_target, sc_weight=1.0, nslices=nslices;
-        ntrials=5, βs=0.01:0.05:22.0, niters=20, rw_weight=0.2), simplifier=MergeGreedy())
+        ntrials=7, βs=0.01:0.05:22.0, niters=20, rw_weight=2.0), simplifier=MergeGreedy())
     println("Graph size $n, usecuda = $usecuda")
     @show timespace_complexity(gp)
     res = GraphTensorNetworks.big_integer_solve(Int32, 100) do T
@@ -20,12 +20,13 @@ function sequencing(n; writefile, sc_target, usecuda, nslices=1)
 end
 
 # patch
-const L = parse(Int, ARGS[1])
-const DEVICE = parse(Int, ARGS[2])
+const DEVICE = parse(Int, ARGS[1])
 
 if DEVICE >= 0
     CUDA.device!(DEVICE)
 end
 
-println("computing L = $L")
-@time sequencing(L; writefile=true, sc_target=28, usecuda=DEVICE>=0, nslices=L-27)
+for L=31:40
+    println("computing L = $L")
+    @time sequencing(L; writefile=true, sc_target=27, usecuda=DEVICE>=0, nslices=L-27)
+end
