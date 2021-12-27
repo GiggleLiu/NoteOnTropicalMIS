293c8ce4c8f3014adee23e61d42d4396b09d9e84
GiggleLiu <cacate0129@gmail.com>
2021-12-27 07:05:59 +0000

    update benchmarks

--- benchmarks/benchmarks.jl
+++ benchmarks/benchmarks.jl
@@ -1,5 +1,5 @@
 using GraphTensorNetworks, DelimitedFiles, TropicalGEMM
-using LightGraphs
+using Graphs
 using BenchmarkTools, Random
 using LinearAlgebra
 using CUDA
@@ -7,18 +7,20 @@ using Comonicon
 
 BLAS.set_num_threads(1)
 
-function case_r3(n, k=3; seed=2)
+function case_r3(n, k=3; seed=2, maxsc)
     # generate a random regular graph of size 100, degree 3
-    graph = (Random.seed!(seed); LightGraphs.random_regular_graph(n, k))
+    graph = (Random.seed!(seed); Graphs.random_regular_graph(n, k))
     @assert length(connected_components(graph)) == 1  # connected graph
     # optimize the contraction order using KaHyPar + Greedy
     optcode = Independence(graph; optimizer=TreeSA(sc_target=0, sc_weight=1.0, ntrials=10, βs=0.01:0.05:25.0, niters=20, rw_weight=2.0), simplifier=MergeGreedy())
     tw = timespace_complexity(optcode)[2]
     @info "n = $n, tw = $tw"
-    if tw > 28
-        optcode = Independence(graph; optimizer=TreeSA(sc_target=28, sc_weight=1.0, ntrials=10, βs=0.01:0.05:25.0, niters=20, rw_weight=2.0, nslices=tw-28), simplifier=MergeGreedy())
+    if tw > maxsc
+        optcode = Independence(graph; optimizer=TreeSA(sc_target=maxsc, sc_weight=1.0, ntrials=10, βs=0.01:0.05:25.0, niters=20, rw_weight=2.0, nslices=Int(tw-maxsc)), simplifier=MergeGreedy())
+        tw = timespace_complexity(optcode)[2]
+        @info "sliced: n = $n, tw = $tw"
     end
-    return optcode, tw
+    return optcode
 end
 
 # setup global arguments
@@ -55,8 +57,8 @@ function runcase(cases; task = :maxsize, usecuda = false)
                    output_file=joinpath(@__DIR__, "data", "$(task)-r3-$(usecuda ? "GPU" : "CPU").dat"))
 end
 
-function generate_instances(nmax::Int)
-    cases = [case_r3(n, 3; seed=2) for n=10:10:nmax]
+function generate_instances(nmax::Int, maxsc::Int)
+    cases = [case_r3(n, 3; seed=2, maxsc=maxsc) for n=10:10:nmax]
             # [case_r3(n, 3; seed=2, treewidth=s) for (n, s) in [
            # (10, 3), (20, 4), (40, 5), (50, 8), (60, 8), (70, 8), (80, 10), (90, 13), (100, 13),
            # (110, 15), (120, 15), (130, 13), (140, 17), (150, 18), (160, 20), (170, 19), (180, 24), (190, 24), (200, 25),
@@ -64,14 +66,14 @@ function generate_instances(nmax::Int)
     return cases
 end
 
-@cast function run_cpu()
+@cast function cpu()
     truncatedict = Dict([string(task)=>ntruncate for (task, ntruncate) in [
             ("counting_sum", 0), ("size_max", 0), ("counting_max", 0), ("counting_max2", 0),
             ("counting_all", 3), ("counting_all_(fft)", 0), ("counting_all_(finitefield)", 0),
             ("config_max", 0), ("configs_max",5), ("configs_all", 16), ("configs_max2", 9), ("config_max_(bounded)", 0), ("configs_max_(bounded)", 0)
             ]])
 
-    cases = generate_instances(200)
+    cases = generate_instances(200, 27)
     run = false
     for TASK in keys(truncatedict)
         println(TASK)
@@ -82,8 +84,8 @@ end
     end
 end
 
-@cast function run_gpu()
-    cases = generate_instances(250)
+@cast function gpu()
+    cases = generate_instances(250, 27)
     for TASK in ["counting_sum", "size_max", "counting_max", "counting_max2",
         "counting_all_(fft)", 
 	"counting_all_(finitefield)",
@@ -91,6 +93,24 @@ end
         ]
         runcase(cases; task=TASK, usecuda=true)
     end
+end
+
+@cast function tw()
+    cases = generate_instances(250, 100)
+    tcscs = zeros(length(cases), 2)
+    for i=1:length(cases)
+        tcscs[i,:] .= timespace_complexity(cases[i])
+    end
+    writedlm(joinpath(@__DIR__, "data", "treewidth.dat"), tcscs)
+end
+
+@cast function tcsc()
+    cases = generate_instances(250, 27)
+    tcscs = zeros(length(cases), 2)
+    for i=1:length(cases)
+        tcscs[i,:] .= timespace_complexity(cases[i])
+    end
+    writedlm(joinpath(@__DIR__, "data", "tcsc.dat"), tcscs)
 end
 
 @main
--- benchmarks/benchmarks_maximal.jl
+++ benchmarks/benchmarks_maximal.jl
@@ -1,5 +1,5 @@
-using GraphTensorNetworks, DelimitedFiles
-using LightGraphs
+using GraphTensorNetworks, DelimitedFiles, TropicalGEMM
+using Graphs
 using BenchmarkTools, Random
 using LinearAlgebra
 using CUDA
@@ -7,24 +7,20 @@ using OMEinsumContractionOrders
 
 BLAS.set_num_threads(1)
 
-function case_r3(n, k=3; sc_target, seed=2)
+function case_r3(n, k=3; maxsc, seed=2)
     # generate a random regular graph of size 100, degree 3
-    graph = (Random.seed!(seed); LightGraphs.random_regular_graph(n, k))
+    graph = (Random.seed!(seed); Graphs.random_regular_graph(n, k))
     @assert length(connected_components(graph)) == 1  # connected graph
     # optimize the contraction order using KaHyPar + Greedy
-    #optcode = MaximalIndependence(graph; optmethod=:kahypar, sc_target=sc_target, max_group_size=20, imbalances=0:0.001:1)
-    optcode = MaximalIndependence(graph; optmethod=:tree, sc_target=sc_target, sc_weight=1.0, ntrials=20, βs=0.02:0.05:15.0, niters=50, initializer=:greedy)
+    optcode = MaximalIndependence(graph; optimizer=TreeSA(sc_target=0, sc_weight=1.0, ntrials=10, βs=0.01:0.05:25.0, niters=20, rw_weight=2.0), simplifier=MergeGreedy())
+    tw = timespace_complexity(optcode)[2]
+    @info "n = $n, tw = $tw"
+    if tw > maxsc
+        optcode = MaximalIndependence(graph; optimizer=TreeSA(sc_target=maxsc, sc_weight=1.0, ntrials=10, βs=0.01:0.05:25.0, niters=20, rw_weight=2.0, nslices=Int(tw-maxsc)), simplifier=MergeGreedy())
+        tw = timespace_complexity(optcode)[2]
+        @info "sliced: n = $n, tw = $tw"
+    end
     return optcode
-end
-
-# setup global arguments
-const GRAPH = length(ARGS) >= 1 ? ARGS[1] : "r3"
-const TASK = length(ARGS) >= 2 ? ARGS[2] : "counting_sum"
-const DEVICE = length(ARGS) >= 3 ? parse(Int, ARGS[3]) : -1
-
-if DEVICE >= 0
-    CUDA.device!(DEVICE)
-    Base.ndims(::Base.Broadcast.Broadcasted{CUDA.CuArrayStyle{0}}) = 0
 end
 
 function run_benchmarks(cases; output_file)
@@ -46,32 +42,20 @@ function run_benchmarks(cases; output_file)
     writedlm(output_file, times)
 end
 
-function runcase(;
-        case_set=:r3,
-        task,
-        usecuda = false,
-        ntruncate = 0,  # truncate benchmark cases
-        seed=2
-    )
-    if case_set == :r3
-        cases = [case_r3(n, 3; seed=seed, sc_target=s) for (n, s) in [
+const truncatedict = Dict([string(task)=>ntruncate for (task, ntruncate) in [
+        ("counting_sum", 0), ("counting_all_(finitefield)", 0), ("configs_all", 3), ("bron", 4),
+       ]])
+
+@main function runcase(task, usecuda, ntruncate=truncatedict[task], maxsc=27, seed=2)
+    if task != "bron"
+        cases = [case_r3(n, 3; seed=seed, maxsc=maxsc) for (n, s) in [
             (10, 6), (20, 8), (30, 9), (40, 11), (50, 16), (60, 17), (70, 16), (80, 20), (90, 26), (100, 26),
         ][1:end-ntruncate]]
         run_benchmarks([("n$(10*i)", ()->(usecuda ? (CUDA.@sync solve(case, replace(task, "_"=>" "); usecuda=true)) : solve((@show length(GraphTensorNetworks.labels(case.code)); case), replace(task, "_"=>" "); usecuda=false))) for (i, case) in enumerate(cases)],
-                    output_file=joinpath(@__DIR__, "data", "maximal-$(task)-$(case_set)-$(usecuda ? "GPU" : "CPU").dat"))
-    elseif case_set == :r3bk
-        graphs = [(Random.seed!(seed); LightGraphs.random_regular_graph(i*10, 3)) for i=1:10-ntruncate]
+                    output_file=joinpath(@__DIR__, "data", "maximal-$(task)-r3-$(usecuda ? "GPU" : "CPU").dat"))
+    else
+        graphs = [(Random.seed!(seed); Graphs.random_regular_graph(i*10, 3)) for i=1:10-ntruncate]
         run_benchmarks([("n$(10*i)", ()->maximal_cliques(complement(g))) for (i, g) in enumerate(graphs)],
-                    output_file=joinpath(@__DIR__, "data", "maximal-$(task)-$(case_set)-$(usecuda ? "GPU" : "CPU").dat"))
+                    output_file=joinpath(@__DIR__, "data", "maximal-$(task)-r3-$(usecuda ? "GPU" : "CPU").dat"))
     end
-
 end
-
-const truncatedict = Dict(
-    "r3"=>Dict([string(task)=>ntruncate for (task, ntruncate) in [
-        ("counting_sum", 0), ("counting_all_(finitefield)", 0), ("configs_all", 3),
-        ]]),
-    "r3bk"=>Dict(["configs_all"=>4])
-    )
-
-runcase(case_set=Symbol(GRAPH), task=TASK, usecuda=DEVICE>=0, ntruncate=truncatedict[GRAPH][TASK])
