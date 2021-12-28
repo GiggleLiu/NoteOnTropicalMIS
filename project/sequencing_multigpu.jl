a37f565b0f347d689598bb127ac7168535d5a0a6
GiggleLiu <cacate0129@gmail.com>
2021-12-28 07:38:21 +0000

    update data

--- project/sequencing_multigpu.jl
+++ project/sequencing_multigpu.jl
@@ -65,20 +65,29 @@ end
 function sequencing(n; writefile, sc_target, usecuda, nslices=1)
     g = square_lattice_graph(trues(n, n))
     gp = Independence(g; optimizer=TreeSA(sc_target=sc_target, sc_weight=1.0, nslices=nslices;
-        ntrials=10, βs=0.01:0.05:22.0, niters=20, rw_weight=2.0), simplifier=MergeGreedy())
+        ntrials=20, βs=0.01:0.05:25.0, niters=50, rw_weight=1.0), simplifier=MergeGreedy())
     println("Graph size $n, usecuda = $usecuda")
     @show timespace_complexity(gp)
+    return
     res = GraphTensorNetworks.big_integer_solve(Int32, 100) do T
         @info "T = $T"
-        @time Array(GraphTensorNetworks.contractx(gp, one(T); usecuda=usecuda))
+        filename = joinpath(@__DIR__, "data", "$n-$(GraphTensorNetworks.modulus(one(T))).dat")
+        if isfile(filename)
+            fill(readdlm(filename)[])
+        else
+            @time res = Array(GraphTensorNetworks.contractx(gp, one(T); usecuda=usecuda))
+            writedlm(filename, res[].val)
+            res
+        end
     end
     @show res
     ofname = joinpath(@__DIR__, "data", "$n.dat")
     writefile && writedlm(ofname, res)
 end
 
-Random.seed!(2)
-for L=38
+# current best = 9
+Random.seed!(parse(Int, ARGS[2]))
+for L=39
     println("computing L = $L")
     @time sequencing(L; writefile=true, sc_target=28, usecuda=USECUDA, nslices=L-28)
 end
