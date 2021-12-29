09b56d3d479a9ef939d0b640a998168242384a8f
GiggleLiu <cacate0129@gmail.com>
2021-12-29 10:10:54 +0000

    update data

--- project/sequencing_multigpu.jl
+++ project/sequencing_multigpu.jl
@@ -84,8 +84,8 @@ end
 
 # current best for 38 = 204: 49.27
 # current best for 39 = 7: 49.27
-Random.seed!(33) #parse(Int, ARGS[2]))
-for L=39
+for L=38:39
+    Random.seed!(L == 38 ? 204 : 33)
     println("computing L = $L")
     @time sequencing(L; writefile=true, sc_target=28, usecuda=USECUDA, nslices=L-28, process_device_map=process_device_map)
 end
