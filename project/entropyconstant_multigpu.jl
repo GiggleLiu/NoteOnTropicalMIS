6214bdf27467d5c7714d2836f737805d5fd9e0c0
GiggleLiu <cacate0129@gmail.com>
2022-01-04 20:33:01 +0000

    update

--- project/entropyconstant_multigpu.jl
+++ project/entropyconstant_multigpu.jl
@@ -1,4 +1,4 @@
-using Distributed, CUDA, GraphTensorNetworks, Random, DelimitedFiles, GraphTensorNetworks.OMEinsumContractionOrders, GraphTensorNetworks.OMEinsum
+using Distributed, CUDA, GraphTensorNetworks, Random, DelimitedFiles, GraphTensorNetworks.OMEinsumContractionOrders, GraphTensorNetworks.OMEinsum, StatsBase
 USECUDA = parse(Bool, ARGS[1])
 @show USECUDA
 const gpus = collect(devices())
@@ -9,7 +9,7 @@ const process_device_map = Dict(zip(procs, gpus))
 
 @everywhere begin
 using GraphTensorNetworks, Random, GraphTensorNetworks.OMEinsumContractionOrders, GraphTensorNetworks.OMEinsum
-using CUDA
+using CUDA, StatsBase
 CUDA.allowscalar(false)
 using DelimitedFiles
 
@@ -36,6 +36,12 @@ function multiprocess_run(func, inputs::AbstractVector{T}) where T
     return Any[take!(results) for i=1:n]
 end
 
+function generate_mask(Nx::Int, Ny::Int, natoms::Int)
+    mask = zeros(Bool,Nx, Ny)
+    mask[sample(1:Nx*Ny, natoms; replace=false)] .= true
+    return mask
+end
+
 function multigpu_contract(se::SlicedEinsum{LT,ET}, xs::Tuple; size_info = nothing, process_device_map::Dict) where {LT, ET}
     length(se.slicing) == 0 && return se.eins(xs...; size_info=size_info)
     size_dict = size_info===nothing ? Dict{OMEinsum.labeltype(se),Int}() : copy(size_info)
@@ -59,28 +65,64 @@ function multigpu_contract(se::SlicedEinsum{LT,ET}, xs::Tuple; size_info = nothi
     return res
 end
 
-function sequencing(graph, n; writefile, sc_target, usecuda, nslices=1, process_device_map)
-    if graph == "diag"
-        g = diagonal_coupled_graph(trues(n, n))
-    elseif graph == "square"
-        g = square_lattice_graph(trues(n, n))
+function countall(grid, n; sc_target, usecuda, nslices, process_device_map, mask)
+    if grid == "diag"
+        g = diagonal_coupled_graph(mask)
+    elseif grid == "square"
+        g = square_lattice_graph(mask)
     end
-    gp = Independence(g; optimizer=TreeSA(sc_target=sc_target, sc_weight=1.0, nslices=nslices;
-        ntrials=4, βs=0.01:0.05:25.0, niters=50, rw_weight=1.0), simplifier=MergeGreedy())
+    if n >= 25
+        gp = Independence(g; optimizer=TreeSA(sc_target=sc_target, sc_weight=1.0, nslices=nslices;
+            ntrials=3, βs=0.01:0.05:25.0, niters=6, rw_weight=2.0), simplifier=MergeGreedy())
+    else
+        gp = Independence(g)
+    end
     println("Graph size $n, usecuda = $usecuda")
     @show timespace_complexity(gp)
     xs = GraphTensorNetworks.generate_tensors(x->1.0, gp)
-    @time res = multigpu_contract(gp.code, (xs...,); process_device_map=process_device_map)
-    @show res
+    if n>= 25
+        @time res = multigpu_contract(gp.code, (xs...,); process_device_map=process_device_map)
+    else
+        @time res = gp.code(xs...)
+    end
+    return res[]
+end
+
+function countall_multiple(graph, n; writefile, sc_target, usecuda, nslices, process_device_map)
+    if graph == "diag"
+        grid = "diag"
+        ρ = 1.0
+        nrepeat = 1
+    elseif graph == "square"
+        grid = "square"
+        ρ = 1.0
+        nrepeat = 1
+    elseif graph == "diag-0.8"
+        grid = "diag"
+        ρ = 0.8
+        nrepeat = 1000
+    elseif graph == "square-0.8"
+        grid = "square"
+        ρ = 0.8
+        nrepeat = 1000
+    end
+    res = zeros(nrepeat)
+    for i=1:nrepeat
+        mask = generate_mask(n, n, round(Int,n^2*ρ))
+        println("$i: graph $grid, n=$n, natoms=$(sum(mask))")
+        res[i] = countall(grid, n; sc_target=sc_target, usecuda=usecuda, nslices=nslices, process_device_map=process_device_map, mask=mask)
+    end
     ofname = joinpath(@__DIR__, "data", "$graph", "entropyconstant-$n.dat")
+    !ispath(dirname(ofname)) && mkdir(dirname(ofname))
     writefile && writedlm(ofname, res)
 end
 
 # current best for 38 = 204: 49.27
 # current best for 39 = 7: 49.27
-for L=27:38
-    Random.seed!(5)
-    graph = "diag"
+#for L=32:-1:25
+for L=7:24
+    Random.seed!(2)
+    graph = "diag-0.8"
     println("computing L = $L")
-    @time sequencing(graph, L; writefile=true, sc_target=28, usecuda=USECUDA, nslices=L-26, process_device_map=process_device_map)
+    @time countall_multiple(graph, L; writefile=true, sc_target=28, usecuda=USECUDA, nslices=0, process_device_map=process_device_map)
 end
---
+++ project/miscounting.jl
@@ -0,0 +1,40 @@
+using Distributed, CUDA, GraphTensorNetworks, Random, DelimitedFiles, GraphTensorNetworks.OMEinsumContractionOrders, GraphTensorNetworks.OMEinsum
+USECUDA = parse(Bool, ARGS[1])
+@show USECUDA
+
+function countmax(graph, n; writefile, sc_target, usecuda, nslices)
+    mask = trues(n, n)
+    if graph == "diag"
+        g = diagonal_coupled_graph(mask)
+    elseif graph == "square"
+        g = square_lattice_graph(mask)
+    end
+    gp = Independence(g; optimizer=TreeSA(sc_target=sc_target, sc_weight=1.0, nslices=nslices;
+        ntrials=4, βs=0.01:0.05:25.0, niters=50, rw_weight=1.0), simplifier=MergeGreedy())
+    println("Graph size $n, usecuda = $usecuda")
+    @show timespace_complexity(gp)
+    res = GraphTensorNetworks.big_integer_solve(Int32, 100) do T
+        @info "T = $T"
+        filename = joinpath(@__DIR__, "data", graph, "maxcount-$n-$(GraphTensorNetworks.modulus(one(T))).dat")
+        if isfile(filename)
+            fill(T(readdlm(filename)[]))
+        else
+            xs = GraphTensorNetworks.generate_tensors(x->CountingTropical(Int32(1), one(T)), gp)
+            @time res = gp.code(xs...)
+            writedlm(filename, res[].c.val)
+            fill(res[].c)
+        end
+    end
+    @show res
+    ofname = joinpath(@__DIR__, "data", graph, "maxcount-$n.dat")
+    writefile && writedlm(ofname, res)
+end
+
+# current best for 38 = 204: 49.27
+# current best for 39 = 7: 49.27
+for L=2:30
+    Random.seed!(2)
+    graph = "diag"
+    println("computing graph $graph, L = $L")
+    @time countmax(graph, L; writefile=true, sc_target=28, usecuda=USECUDA, nslices=0)
+end
