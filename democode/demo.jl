7f7bf7e081e15d7354af534923ba0513a9d61cee
Wang-Shengtao <Wang-Shengtao@users.noreply.github.com>
2022-01-22 10:40:01 -0500

    grammar fixes for appendix A

--- democode/demo.jl
+++ democode/demo.jl
@@ -9,16 +9,16 @@ graph = (Random.seed!(2); Graphs.random_regular_graph(50, 3))
 code = EinCode(([minmax(e.src,e.dst) for e in Graphs.edges(graph)]..., # labels for edge tensors
                 [(i,) for i in Graphs.vertices(graph)]...), ())        # labels for vertex tensors
 
-# an einsum contraction without contraction order specified is called `EinCode`,
-# an einsum contraction has contraction order (specified as a tree structure) is called `NestedEinsum`.
-# assign each label a dimension-2, it will be used in contraction order optimization
-# `uniquelabels` function extracts tensor labels into a vector.
+# an einsum contraction without a contraction order specified is called `EinCode`,
+# an einsum contraction having a contraction order (specified as a tree structure) is called `NestedEinsum`.
+# assign each label a dimension-2, it will be used in the contraction order optimization
+# `uniquelabels` function extracts the tensor labels into a vector.
 size_dict = Dict([s=>2 for s in uniquelabels(code)])
-# optimize the contraction order using the `TreeSA` method, target space complexity is 2^17
+# optimize the contraction order using the `TreeSA` method; the target space complexity is 2^17
 optimized_code = optimize_code(code, size_dict, TreeSA())
 println("time/space complexity is $(OMEinsum.timespace_complexity(optimized_code, size_dict))")
 
-# a function for computing independence polynomial
+# a function for computing the independence polynomial
 function independence_polynomial(x::T, code) where {T}
 	xs = map(getixsv(code)) do ix
         # if the tensor rank is 1, create a vertex tensor.
@@ -29,9 +29,9 @@ function independence_polynomial(x::T, code) where {T}
 	code(xs...)
 end
 
-########## COMPUTING MAXIMUM INDEPENDENT SET SIZE AND ITS DEGENERACY ###########
+########## COMPUTING THE MAXIMUM INDEPENDENT SET SIZE AND ITS COUNTING/DEGENERACY ###########
 
-# using Tropical numbers to compute the MIS size and MIS degeneracy.
+# using Tropical numbers to compute the MIS size and the MIS degeneracy.
 using TropicalNumbers
 mis_size(code) = independence_polynomial(TropicalF64(1.0), code)[]
 println("the maximum independent set size is $(mis_size(optimized_code).n)")
@@ -39,7 +39,7 @@ println("the maximum independent set size is $(mis_size(optimized_code).n)")
 mis_count(code) = independence_polynomial(CountingTropical{Float64,Float64}(1.0, 1.0), code)[]
 println("the degeneracy of maximum independent sets is $(mis_count(optimized_code).c)")
 
-########## COMPUTING INDEPENDENCE POLYNOMIAL ###########
+########## COMPUTING THE INDEPENDENCE POLYNOMIAL ###########
 
 # using Polynomial numbers to compute the polynomial directly
 using Polynomials
@@ -95,11 +95,11 @@ improved_counting(sequences) = map(yi->Mods.CRT(yi...), zip(sequences...))
 
 println("the independence polynomial (finite field) is $(independence_polynomial_finitefield(optimized_code))")
 
-########## FINDING OPTIMAL CONFIGURATIONS ###########
+########## FINDING MIS CONFIGURATIONS ###########
 
 # define the set algebra
 struct ConfigEnumerator{N}
-    # NOTE: BitVector is dynamic, can be very slow, check our repo for the static version
+    # NOTE: BitVector is dynamic and it can be very slow; check our repo for the static version
     data::Vector{BitVector}
 end
 function Base.:+(x::ConfigEnumerator{N}, y::ConfigEnumerator{N}) where {N}
@@ -122,7 +122,7 @@ struct ConfigSampler{N}
     data::BitVector
 end
 
-function Base.:+(x::ConfigSampler{N}, y::ConfigSampler{N}) where {N}  # biased sampling: return `x`, maybe using random sampler is better.
+function Base.:+(x::ConfigSampler{N}, y::ConfigSampler{N}) where {N}  # biased sampling: return `x`
     return x  # randomly pick one
 end
 function Base.:*(x::ConfigSampler{L}, y::ConfigSampler{L}) where {L}
@@ -132,10 +132,10 @@ end
 Base.zero(::Type{ConfigSampler{N}}) where {N} = ConfigSampler{N}(trues(N))
 Base.one(::Type{ConfigSampler{N}}) where {N} = ConfigSampler{N}(falses(N))
 
-# enumerate all configurations if `all` is true, compute one otherwise.
-# a configuration is stored in the data type of `StaticBitVector`, it uses integers to represent bit strings.
-# `ConfigTropical` is defined in `TropicalNumbers`. It has two fields, tropical number `n` and optimal configuration `config`.
-# `CountingTropical{T,<:ConfigEnumerator}` is a simple stores configurations instead of simple counting.
+# enumerate all configurations if `all` is true; compute one otherwise.
+# a configuration is stored in the data type of `StaticBitVector`; it uses integers to represent bit strings.
+# `ConfigTropical` is defined in `TropicalNumbers`. It has two fields: tropical number `n` and optimal configuration `config`.
+# `CountingTropical{T,<:ConfigEnumerator}` stores configurations instead of simple counting.
 function mis_config(code; all=false)
     # map a vertex label to an integer
     vertex_index = Dict([s=>i for (i, s) in enumerate(uniquelabels(code))])
@@ -159,5 +159,5 @@ end
 
 println("one of the optimal configurations is $(mis_config(optimized_code; all=false)[].c.data)")
 
-# enumerating configurations directly can be very slow, please check the bounding version in our Github repo.
+# direct enumeration of configurations can be very slow; please check the bounding version in our Github repo.
 println("all optimal configurations are $(mis_config(optimized_code; all=true)[].c)")
